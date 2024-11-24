842912dd7bb91685534e5f69a4d1dc97647e50e3
Etni Castro <etnicastro9@gmail.com>
2024-11-23 21:38:07 -0600

    tercer commit

--- app/Http/Controllers/AdafruitController.php
+++ app/Http/Controllers/AdafruitController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Sensor;
 
 class AdafruitController extends Controller
 {
-    //
+    public function obtener(){
+
+        $response = Http::get('https://io.adafruit.com/api/v2/zaniwonzzzzz/feeds/led/data');
+
+        if($response->successful()){
+            $data = $response->json();
+            return response()->json($data);
+        }
+    }
+
+    public function crear(request $request){
+        $led = new Sensor();
+        $led->value = $request->value;
+        $led->save();
+
+        $key = config('services.adafruit.key');
+
+        $response = Http::WithHeaders([
+            'X-AIO-Key' => $key]) ->post('https://io.adafruit.com/api/v2/zaniwonzzzzz/feeds/led/data', [
+                'value' => $request->value
+            ]);
+
+            if ($response->successful()) {
+                return response()->json(['message' => 'datos enviados correctamente']);
+            } 
+    }
 }
