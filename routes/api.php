19cd83eb2b68f2035bae118c5c4452883b1f2de3
Etni Castro <etnicastro9@gmail.com>
2024-12-09 13:16:08 -0600

    treintaysiete commit

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::group([
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+    Route::get('me', [AuthController::class, 'me']);
     
 }); 
 
