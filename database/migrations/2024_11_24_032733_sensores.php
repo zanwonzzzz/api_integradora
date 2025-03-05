70ecd1c1a25c7425319d322b4e402abc441c8838
Etni Castro <etnicastro9@gmail.com>
2025-03-04 18:12:54 -0600

    ochentaycuatro commit

--- database/migrations/2024_11_24_032733_sensores.php
+++ database/migrations/2024_11_24_032733_sensores.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -19,6 +20,14 @@ return new class extends Migration
             $table->softDeletes();
             $table->timestamps();
         });
+
+        DB::table('sensores')->insert([
+            ['Nombre_Sensor' => 'gas'],
+            ['Nombre_Sensor' => 'temperatura'],
+            ['Nombre_Sensor' => 'movimiento'],
+            ['Nombre_Sensor' => 'sonido'],
+            ['Nombre_Sensor' => 'luz'],
+        ]);
     }
 
     /**
