7ea331e2fc11b735d0e10b1ebc75245286dacd67
DanSoryu <162482741+DanSoryu@users.noreply.github.com>
2025-03-07 17:56:29 -0600

    ok

--- routes/api.php
+++ routes/api.php
@@ -157,5 +157,5 @@ Route::get('/cronjobs2', [CronJobController::class, 'CronJobParaDatosNuevos'])->
 Route::get('/sensorsitos/{id}',[AdafruitController::class,'BorrarSensores'])->middleware('auth.jwt');
 
 //DATA SENSORES 
-Route::get('/sensor-data/{id}', [SensorDataController::class, 'index']);
+Route::get('/sensor-data/{id}', [SensorDataController::class, 'index'])->middleware('auth.jwt');
 Route::post('/sensor-data', [SensorDataController::class, 'store']);
