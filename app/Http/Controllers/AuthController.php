5d1bd2b663cde27995582dd534df0e9c1f4a1819
Victor Gael <ack3stx10@gmail.com>
2025-04-05 11:49:59 -0600

    oswi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -174,18 +174,18 @@ class AuthController extends Controller
 
     public function ActualizarUsuario(Request $request) {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|min:3|max:50',
-            'password' => 'required|string|min:6'
+            'name' => 'nullable|string|min:3|max:50', 
+            'password' => 'nullable|string|min:6'    
         ]);
     
-        // if ($validator->fails()) {
-        //     return response()->json($validator->errors(), 422);
-        // }
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
     
         $user = auth()->user();
     
-        $isNameUnchanged = $request->name === $user->name;
-        $isPasswordUnchanged = Hash::check($request->password, $user->password);
+        $isNameUnchanged = $request->name ? $request->name === $user->name : true;
+        $isPasswordUnchanged = $request->password ? Hash::check($request->password, $user->password) : true;
     
         if ($isNameUnchanged && $isPasswordUnchanged) {
             return response()->json([
@@ -193,11 +193,11 @@ class AuthController extends Controller
             ], 422);
         }
     
-        if (!$isNameUnchanged) {
+        if ($request->name && !$isNameUnchanged) {
             $user->name = $request->name;
         }
     
-        if (!$isPasswordUnchanged) {
+        if ($request->password && !$isPasswordUnchanged) {
             $user->password = bcrypt($request->password);
         }
     
